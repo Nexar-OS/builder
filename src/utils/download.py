e7454e961e8322c7d4c2d3ed9e504d95a9d373ea
c4vxl <manage@c4vxl.de>
2026-08-06 11:22:49 +0200

    Added download utilities

---
+++ src/utils/download.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import requests
+
+def download_url(url: str, dest: Path, chunk_size: int = 8192) -> None:
+    """Download a file from a URL into a specific path on disk.
+
+    Args:
+        url (str): The url to download the file from.
+        dest (Path): The final path where the file should be placed at (including filename).
+    """
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    with dest.open("wb") as file:
+        for chunk in response.iter_content(chunk_size=chunk_size):
+            if chunk:
+                file.write(chunk)

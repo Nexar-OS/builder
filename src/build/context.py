64b8e5e8f0a2fa1ad021af4b2bf0e7fd3083a1a0
c4vxl <manage@c4vxl.de>
2026-08-06 15:16:32 +0200

    Added sandboxed shell to BuildContext

--- src/build/context.py
+++ src/build/context.py
@@ -1,8 +1,14 @@
+import subprocess
 from pathlib import Path
 from dataclasses import dataclass
 
 from .machine import MachineSpec
-from toolchain.toolchain import Toolchain
+
+import typing
+if typing.TYPE_CHECKING:
+    from toolchain.toolchain import Toolchain
+
+from utils.logger import debug
 
 @dataclass
 class BuildContext:
@@ -26,9 +32,54 @@ class BuildContext:
     build_machine: MachineSpec
     target_machine: MachineSpec
 
-    toolchain: Toolchain
+    toolchain: "Toolchain"
 
     cross_toolchain_dir: Path
     cross_toolchain_sysroot: Path
 
     num_jobs: int
+
+    @property
+    def env(self) -> dict[str, str]:
+        """
+        Return the toolchains environment.
+        """
+        return self.toolchain.env or dict()
+
+    def run(self,
+            cmd: list[str],
+            check: bool = True,
+            use_toolchain_env: bool = True,
+            use_fakeroot: bool = True,
+            **kwargs
+            ):
+        """
+        Execute a shell command sandboxed fakeroot and the build contexts environment.
+
+        Args:
+            cmd (list[str]): The command to invoke.
+            check (bool, optional): Check for errors.
+            use_toolchain_env (bool, optional): If set to False, no custom environment will be used.
+            use_fakeroot (bool, optional): If set to False, no fakeroot will be used.
+        """
+
+        debug(f"> {' '.join(cmd)}")
+
+        if use_toolchain_env:
+            kwargs.setdefault("env", self.env)
+
+        result = subprocess.run(
+            cmd,
+            text=True,
+            capture_output=True,
+            **kwargs
+        )
+
+        if check and result.returncode != 0:
+            raise RuntimeError(
+                f"Command failed: {' '.join(cmd)}\n\n",
+                f"STDOUT: \n{result.stdout}\n\n\n",
+                f"STDERR: \n{result.stderr}"
+            )
+        
+        return result

55c5f7d2065d91e2dc2fb8c4105f5a4293429be4
c4vxl <manage@c4vxl.de>
2026-08-07 18:23:34 +0200

    Added compatibility symlinks to ncurses target recipe

--- src/recipe/target/ncurses.py
+++ src/recipe/target/ncurses.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from build import BuildContext
 from build.system import Autotools
 from recipe import TargetRecipe
@@ -32,3 +33,34 @@ class NcursesRecipe(TargetRecipe):
             f"--build={ctx.build_machine.triple}",
             f"--host={ctx.target_machine.triple}"
         ]
+
+    def post_install(self, ctx: BuildContext, dest_dir: Path | None) -> None:
+        """
+        Create compatibility symlinks for ncursesw.
+        Some recipes might expect a specific version of the library to be present.
+        """
+
+        assert dest_dir
+
+        # Create ncursesw directory for headers
+        ncursesw = dest_dir / "usr/include/ncursesw"
+        ncursesw.mkdir(exist_ok=True, parents=True)
+        for header in [ "curses.h", "term.h", "ncurses.h", "termcap.h" ]:
+            link = ncursesw / header
+            
+            if link.exists() or link.is_symlink():
+                link.unlink()
+            
+            link.symlink_to(f"../{header}")
+        
+        # Create symlinks for wide-character libraries
+        for lib in [ "libform", "libmenu", "libncurses", "libpanel", "libtinfo" ]:
+            wlib = f"{lib}w.so"
+            lib = f"{lib}.so"
+
+            link = dest_dir / "usr/lib" / lib
+
+            if link.exists() or link.is_symlink():
+                link.unlink()
+            
+            link.symlink_to(wlib)

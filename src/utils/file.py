58f414839308e5e4b1b3468433605e7800b442ec
c4vxl <manage@c4vxl.de>
2026-08-07 18:06:11 +0200

    Added merge_trees utility to file utils

--- src/utils/file.py
+++ src/utils/file.py
@@ -9,3 +9,47 @@ def rmtree(path: Path):
         path (Path): The root folder to delete.
     """
     shutil.rmtree(path, ignore_errors=True)
+
+def merge_trees(source: Path, dest: Path, copy: bool = False):
+    """
+    Merge the contents of one directory tree into another.
+
+    - Files and symlinks are moved.
+    - Existing files/symlinks/directories in the destination are overwritten.
+    - Existing destination directories are merged recursively.
+
+    Args:
+        source (Path): Source tree to merge into destination.
+        dest (Path): Destination tree to merge the source into.
+        copy (bool): If True, content will be copied instead of being moved.
+    """
+
+    if not source.is_dir():
+        return
+    
+    dest.mkdir(exist_ok=True, parents=True)
+
+    for item in source.iterdir():
+        dst = dest / item.name
+
+        # Merge real directories recursively
+        if dest.is_dir() and not item.is_symlink():
+            merge_trees(item, dst, copy)
+        
+        else:
+            # Remove whatever is currently at the destination
+            if dst.exists():
+                dst.unlink()
+            
+            # Move or copy file or symlinks without dereferencing it
+            if copy:
+                shutil.copy2(str(item), str(dst), follow_symlinks=False)
+            else:
+                shutil.move(str(item), str(dst))
+    
+    # Finally remove source node
+    if not copy:
+        try:
+            source.rmdir()
+        except OSError:
+            pass

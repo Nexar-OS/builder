ba757be8bc962f4366b303e86ff5f87271003e25
c4vxl <manage@c4vxl.de>
2026-08-08 13:01:08 +0200

    Added pciutils build system

--- src/build/system/__init__.py
+++ src/build/system/__init__.py
@@ -2,3 +2,4 @@ from .buildsystem import BuildSystem
 from .autotools import Autotools
 from .linux_headers import LinuxHeaders
 from .bzip2 import BZip2
+from .pciutils import Pciutils
---
+++ src/build/system/pciutils.py
@@ -0,0 +1,49 @@
+import shutil
+from pathlib import Path
+from .buildsystem import BuildSystem
+from build.context import BuildContext
+
+class Pciutils(BuildSystem):
+    """
+    Abstraction for building pciutils.
+    """
+
+    def prepare(self, ctx: BuildContext, source_dir: Path, build_dir: Path) -> None:
+        # Not needed
+        pass
+
+    def configure(self,
+                  ctx: BuildContext,
+                  source_dir: Path,
+                  build_dir: Path,
+                  config_args: list[str] | None = None):
+        # Not needed
+        pass
+
+    def build(self, ctx: BuildContext, build_dir: Path):
+        """
+        Builds pciutils using ``make``.
+
+        The following command will be executed:
+        ``make PREFIX=/usr HOST=$target CROSS_COMPILE=$target all``
+        """
+
+        ctx.run(
+            [ ctx.toolchain.make, "PREFIX=/usr", f"HOST={ctx.target_machine.triple}", f"CROSS_COMPILE={ctx.target_machine.triple}-", "all" ],
+            cwd=build_dir # build dir is source dir
+        )
+
+    def install(self, ctx: BuildContext, build_dir: Path, dest_dir: Path | None = None):
+        """
+        Install pciutils into the target destination.
+
+        Args:
+            dest_dir (Path | None, optional): Must be passed.
+        """
+        
+        assert dest_dir, "dest_dir argument is required for pciutils"
+
+        ctx.run(
+            [ ctx.toolchain.make, "PREFIX=/usr", f"DESTDIR={str(dest_dir)}", "install" ],
+            cwd=build_dir # build dir is source dir
+        )

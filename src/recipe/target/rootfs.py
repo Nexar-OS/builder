7d34b0928b897768f251abfdc116b66c60a5bbdd
c4vxl <manage@c4vxl.de>
2026-08-16 18:46:03 +0200

    Added minimal population of /etc/ for root user

--- src/recipe/target/rootfs.py
+++ src/recipe/target/rootfs.py
@@ -48,3 +48,32 @@ class RootfsRecipe(TargetRecipe):
                 link.unlink()
             
             link.symlink_to(target)
+        
+
+        # Create minimal /etc/passwd
+        with (dest_dir / "etc/passwd").open("w") as f:
+            f.write("root:x:0:0::/root:/usr/bin/bash\n")
+        
+        # Create minimal /etc/group
+        with (dest_dir / "etc/group").open("w") as f:
+            f.write("root:x:0:")
+        
+        # Create minimal /etc/shadow
+        with (dest_dir / "etc/shadow").open("w") as f:
+            # hash is for password "test"
+            f.write("root:$6$Qqy4FrIwYO/AYIuJ$ZrN10dVJ8p34.JIrsPVfZnO39h9ZRA8WTwAZEcNPKEUUR2Ju8O3zk0lvuXzydbRmduO7YWzi788zLWbxEBsCV/:2000:0:99999:7:::")
+
+        # Create minimal /etc/nsswitch
+        with (dest_dir / "etc/nsswitch").open("w") as f:
+            f.write("passwd: files\n")
+            f.write("group: files\n")
+            f.write("shadow: files\n")
+        
+        # Create minimal /etc/pam.d/login
+        (dest_dir / "etc/pam.d/").mkdir(exist_ok=True, parents=True)
+        with (dest_dir / "etc/pam.d/login").open("w") as f:
+            # f.write("#%PAM-1.0\n\n")
+
+            f.write("auth       required      pam_unix.so\n")
+            f.write("account    required      pam_unix.so\n")
+            f.write("session    required      pam_unix.so\n")

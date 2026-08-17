4582038a066178eb44589b80201f329c7865c376
c4vxl <manage@c4vxl.de>
2026-08-17 14:56:44 +0200

    Updated version of openssl

--- src/recipe/target/openssl.py
+++ src/recipe/target/openssl.py
@@ -1,25 +1,30 @@
-from build import BuildContext
 from build.system import Autotools
-from recipe import TargetRecipe, BuildMethod
+from recipe import TargetRecipe
 from source import TarballSource
 
 class OpenSSLRecipe(TargetRecipe):
     name = "openssl"
-    version = "4.0.1"
+    version = "3.6.3"
 
     copy_to_toolchain = True
+    _export_to_toolchain = [
+        "etc/ssl",
+        "usr"
+    ]
 
     sources = [
         TarballSource(
             name="openssl",
             url=f"https://github.com/openssl/openssl/releases/download/openssl-{version}/openssl-{version}.tar.gz",
-            md5hash="07e316afe26b61e72206b81706b497bb"
+            md5hash="f388d6144fe20b9b2c6bf208280d6ec3"
         )
     ]
 
     build_system = Autotools(
         config_args=[
-            "--prefix=/usr"
+            "--prefix=/usr",
+            "--openssldir=/etc/ssl",
+            "shared"
         ],
         disable_fakeroot=True
     )

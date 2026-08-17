0e40a8724e59573fd9d92ba1a56415081136c58a
c4vxl <manage@c4vxl.de>
2026-08-17 14:05:11 +0200

    Added libedit target recipe

---
+++ src/recipe/target/libedit.py
@@ -0,0 +1,30 @@
+from build import BuildContext
+from build.system import Autotools
+from recipe import TargetRecipe
+from source import TarballSource
+
+class LibeditRecipe(TargetRecipe):
+    name = "libedit"
+    version = "20260512-3.1"
+
+    copy_to_toolchain = True
+
+    sources = [
+        TarballSource(
+            name="libedit",
+            url=f"https://thrysoee.dk/editline/libedit-{version}.tar.gz",
+            md5hash="2067df0d3b79b0f731994ad830d41a6d"
+        )
+    ]
+
+    build_system = Autotools(
+        config_args=[
+            "--prefix=/usr"
+        ]
+    )
+
+    def _config_args(self, ctx: BuildContext) -> list[str]:
+        return [
+            f"--build={ctx.build_machine.triple}",
+            f"--host={ctx.target_machine.triple}"
+        ]

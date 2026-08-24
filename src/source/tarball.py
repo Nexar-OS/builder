e762ef5f3e3b230d1b6c9ee3a6ab4013d06f48b4
c4vxl <manage@c4vxl.de>
2026-08-24 17:03:17 +0200

    Added option to skip top-level stripping to tarball source

--- src/source/tarball.py
+++ src/source/tarball.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 import tarfile
 from utils.file import rmtree
 
-def _extract_flat_tar(tarball: Path, dest: Path):
+def _extract_flat_tar(tarball: Path, dest: Path, strip_top_level_dir: bool = True):
     """
     Extracts a tar archive into a destination directory, flattening
     the archive if it contains a single top-level directory.
@@ -13,18 +13,23 @@ def _extract_flat_tar(tarball: Path, dest: Path):
     Args:
         tarball (Path): The path to the tarfile.
         dest (Path): The destination path to extract the tarfile into.
+        strip_top_level_dir (bool): If set to true, a single top-level-directory will be stripped when extracting.
     """
     with tarfile.open(tarball, "r:*") as tar:
         members = tar.getmembers()
 
-        # Get a list of the top-level directory names
-        top_levels = set( m.name.split("/", 1)[0] for m in members if m.name )
+        if not strip_top_level_dir:
+            strip_prefix = None
+        else:
+            # Get a list of the top-level directory names
+            top_levels = set( m.name.split("/", 1)[0] for m in members if m.name )
 
-        # If there is only one top-level component, strip it in extraction
-        strip_prefix = None
-        if len(top_levels) == 1:
-            strip_prefix = next(iter(top_levels)) + "/"
+            # If there is only one top-level component, strip it in extraction
+            strip_prefix = None
+            if len(top_levels) == 1:
+                strip_prefix = next(iter(top_levels)) + "/"
         
+        # Extract tarball
         for member in members:
             # Flatten member names
             if strip_prefix and member.name.startswith(strip_prefix):
@@ -48,6 +53,13 @@ def _extract_flat_tar(tarball: Path, dest: Path):
 
 @dataclass
 class TarballSource(FileSource):
+    
+    """
+    If set to True, a top-level directory will be stripped when
+    extracting the tarball.
+    """
+    strip_top_level: bool = True
+
     def prepare(self, download_dir: Path, dest_dir: Path):
         """Extract the tarball into an optionally differing destination directory.
 
@@ -56,4 +68,5 @@ class TarballSource(FileSource):
             dest_dir (Path): The destination for the contents of the tarball.
         """
         file = self.local_file(download_dir)
-        _extract_flat_tar(file, dest_dir)
+
+        _extract_flat_tar(file, dest_dir, self.strip_top_level)

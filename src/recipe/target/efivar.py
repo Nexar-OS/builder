15307d8a036ae4dbc34de1e909feff4a98fd6e9a
c4vxl <manage@c4vxl.de>
2026-08-25 19:51:20 +0200

    Added efivar recipe for efibootmgr

---
+++ src/recipe/target/efivar.py
@@ -0,0 +1,25 @@
+from build.system import Autotools
+from recipe import TargetRecipe, BuildMethod
+from source import TarballSource
+
+class EfivarRecipe(TargetRecipe):
+    name = "efivar"
+    version = "39"
+
+    build_method = BuildMethod.IN_SOURCE
+
+    copy_to_toolchain = True
+
+    sources = [
+        TarballSource(
+            name="efivar",
+            url=f"https://github.com/rhboot/efivar/archive/refs/tags/{version}.tar.gz",
+            md5hash="a8fc3e79336cd6e738ab44f9bc96a5aa"
+        )
+    ]
+
+    build_system = Autotools(
+        install_args=[
+            "LIBDIR=/usr/lib64"
+        ]
+    )

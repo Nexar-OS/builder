1cf6a470d7ddfe1507bd6a627075ec4ee73b0637
c4vxl <manage@c4vxl.de>
2026-08-28 17:41:47 +0200

    Added CustomBuildSystem

--- src/builder/build/system/__init__.py
+++ src/builder/build/system/__init__.py
@@ -7,3 +7,4 @@ from .pciutils import Pciutils
 from .linux_kernel import LinuxKernel
 from .nss import NSS
 from .cmake import CMake
+from .custom import CustomBuildSystem
---
+++ src/builder/build/system/custom.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from .buildsystem import BuildSystem
+from builder.build.context import BuildContext
+from dataclasses import dataclass
+
+@dataclass
+class CustomBuildSystem(BuildSystem):
+    """
+    Abstraction for a custom build system.
+    """
+    def __init__(self, 
+                 prepare: str | None = None,
+                 configure: str | None = None,
+                 build: str | None = None,
+                 install: str | None = None,
+                 disable_fakeroot: bool = False,
+                ):
+        self._prepare = prepare
+        self._configure = configure
+        self._build = build
+        self._install = install
+        self.disable_fakeroot = disable_fakeroot        
+
+    def _invoke(self, ctx: BuildContext, script: str | None, dir: Path, env: dict[str, str]):
+        if not script:
+            return
+
+        ctx.run(
+            [ "sh", "-c", script ],
+            cwd=dir,
+            env={ **ctx.env, **env}
+        )
+
+    def prepare(self, ctx: BuildContext, source_dir: Path, build_dir: Path) -> None:
+        """
+        Prepare the cross config file for meson.
+        """
+        self._invoke(ctx, self._prepare, source_dir, {
+            "BUILD": str(build_dir),
+            "SOURCE": str(source_dir)
+        })
+        
+
+    def configure(self,
+                  ctx: BuildContext,
+                  source_dir: Path,
+                  build_dir: Path,
+                  config_args: list[str] | None = None):
+        """
+        Configure the project for building.
+
+        This method invokes the ``_configure`` script.
+
+        Args:
+            ctx (BuildContext): Build context.
+            source_dir (Path): Directory containing the projects source tree.
+            build_dir (Path): Directory where the build will be configured.
+            config_args (list[str] | None, optional): Additional configuration args. Defaults to None.
+        """
+        self._invoke(ctx, self._prepare, build_dir, {
+            "BUILD": str(build_dir),
+            "SOURCE": str(source_dir)
+        })
+        
+        
+    def build(self, ctx: BuildContext, build_dir: Path):
+        """
+        Compile the project using the ``_build`` script.
+
+        Args:
+            ctx (BuildContext): Build context.
+            build_dir (Path): Directory containing the configured build tree.
+        """
+        self._invoke(ctx, self._build, build_dir, {
+            "BUILD": str(build_dir)
+        })
+
+    def install(self, ctx: BuildContext, build_dir: Path, dest_dir: Path | None = None):
+        """
+        Install the compiled artifacts using the passed ``_install`` script.
+
+        Args:
+            ctx (BuildContext): Build context.
+            build_dir (Path): Directory containing the build output.
+            dest_dir (Path | None, optional): Destination override. Defaults to None.
+        """
+        self._invoke(ctx, self._build, build_dir, {
+            "BUILD": str(build_dir),
+            "DESTDIR": str(dest_dir)
+        })

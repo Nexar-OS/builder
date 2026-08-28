2893700f1ce5d6f63a6cb634a510ece841959191
c4vxl <manage@c4vxl.de>
2026-08-28 14:48:33 +0200

    Added data utilities

---
+++ src/builder/utils/data.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+from collections.abc import Mapping
+from typing import Any, TypeVar
+from builder.utils.logger import warn
+from yaml import safe_load
+
+T = TypeVar("T")
+
+
+def retrieve_nested(
+    data: Any,
+    path: str|list[str],
+    expected_type: type[T] | None = None,
+    default: T | None = None
+) -> T | None:
+    """
+    Retrieve a value from a nested mapping using a path.
+
+    Each component of ``path`` is used as a key to traverse the nested mappings.
+
+    Args:
+        data (Any): The mapping to traverse.
+        path (str | list[str]): The path to the desired value.
+        expected_type (type[T] | None, optional): The type the value the retrieved
+                                                  value should have.
+        default (T | None, optional): Value to return if the path does not
+                                      exist or the value has an unexpected type. Defaults to None.
+
+    Returns:
+        _type_: The expected type or ``None``.
+    """
+    current = data
+
+    path = [ path ] if isinstance(path, str) else path
+    for part in path:
+        if not isinstance(current, Mapping):
+            warn(
+                f"cannot access '{part}' in '{path}': "
+                f"expected a mapping, got {type(current).__name__}."
+            )
+
+            return default
+
+        if part not in current:
+            warn(f"path '{path} does not exist.'")
+            return default
+
+        current = current[part]
+
+    if expected_type is not None and not isinstance(current, expected_type):
+        warn(
+            f"path '{path}' has type "
+            f"{type(current).__name__}, expected {expected_type.__name__}."
+        )
+        return default
+    
+    return current
+
+
+def load_yaml(path: Path) -> Any:
+    """
+    Safe loads a yaml file.
+
+    Invokes the pyyaml ``safe_load`` function.
+
+    Args:
+        path (Path): The path to the yaml file.
+
+    Returns:
+        Any: The decoded content.
+    """
+    return safe_load(path.open("r"))

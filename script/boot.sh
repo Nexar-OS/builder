eb592e82e9d497e0326529d59ae07bf1ca6af3dc
c4vxl <manage@c4vxl.de>
2026-08-28 12:10:33 +0200

    Fixed ctrl + c killing vm

--- script/boot.sh
+++ script/boot.sh
@@ -51,5 +51,5 @@ qemu-system-x86_64 \
     -drive file=build/disk.img,format=raw,if=virtio \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
     -drive if=pflash,format=raw,file=build/OVMF_VARS.fd \
-    -serial stdio \
+    -serial mon:stdio \
     -display none

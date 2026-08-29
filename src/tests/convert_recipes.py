c44f833bca0a1d2ab902092d42a80adab7a6fcb9
c4vxl <manage@c4vxl.de>
2026-08-29 11:50:01 +0200

    Added helper script to convert python recipes

---
+++ src/tests/convert_recipes.py
@@ -0,0 +1,89 @@
+from tests.vars import ctx
+
+from builder.build.system import *
+from pathlib import Path
+from builder.recipe import *
+from builder.recipe.target import *
+
+def load_recipes():
+    import importlib
+    import pkgutil
+    import inspect
+
+    recipes = []
+
+    package = importlib.import_module("builder.recipe.target")
+
+    for module_info in pkgutil.iter_modules(package.__path__):
+        if module_info.ispkg:
+            continue
+
+        module = importlib.import_module(f"{package.__name__}.{module_info.name}")
+
+        for name, obj in inspect.getmembers(module, inspect.isclass):
+            if (
+                    name.endswith("Recipe")
+                    and obj is not BuildRecipe
+                    and issubclass(obj, TargetRecipe)
+                    and obj.__module__ == module.__name__
+                ):
+                recipes.append(obj(ctx))
+    
+    return recipes
+
+existing_recipes = [
+    f.name.removesuffix(".yaml")
+    for f in Path("src/recipe/").iterdir()
+    if f.is_file() and f.name.endswith(".yaml")
+]
+
+recipes = load_recipes()
+recipes = [ r for r in recipes if not r.name in existing_recipes ]
+
+dst = Path("build/exported_recipes/")
+dst.mkdir(exist_ok=True, parents=True)
+
+preset = Path("src/recipe/.example").read_text()
+
+def export_recipe(recipe: TargetRecipe) -> str | None:
+    if not isinstance(recipe.build_system, Autotools):
+        return None
+
+    config_args = (recipe.build_system.config_args or []) + recipe._config_args(ctx)
+    config_args = [ arg for arg in config_args if not any(arg.startswith(x) for x in [ "--host", "--build", "--target", "--sysroot" ]) ]
+    config_args = "\n      - ".join(config_args) + "\n"
+
+
+    build_args = recipe.build_system.build_args or []
+    build_args = "\n      - ".join(build_args) + "\n"
+
+    install_args = recipe.build_system.install_args or []
+    install_args = "\n      - ".join(install_args) + "\n"
+
+    return preset \
+        .replace("<name>", recipe.name) \
+        .replace("<version>", recipe.version) \
+        .replace("<source_type>", type(recipe.sources[0]).__name__.lower().removesuffix('source')) \
+        .replace("<source_url>", recipe.sources[0].url.replace(recipe.version, '${version}')) \
+        .replace("<source_name>", recipe.sources[0].name) \
+        .replace("<source_hash>", getattr(recipe.sources[0], 'md5hash')) \
+        .replace("<config_args>", config_args) \
+        .replace("<build_args>", build_args) \
+        .replace("<install_args>", install_args) \
+        .replace("<build_method>", recipe.build_method.name) \
+        .replace("<build_system>", type(recipe.build_system).__name__.lower()) \
+        .replace("<disable_fakeroot>", f"{recipe.build_system.disable_fakeroot}") \
+        .replace("disable_fakeroot: False\n", "") \
+        .replace("config_args:\n      - \n", "config_args:") \
+        .replace("build_args:\n      - \n", "build_args:") \
+        .replace("install_args:\n      - \n", "install_args:") \
+
+for r in recipes:
+    path: Path = dst / (r.name + ".yaml")
+    exported = export_recipe(r)
+
+    if not exported:
+        print(f"[WARNING]: Failed to export '{r.name}'")
+        continue
+
+    path.write_text(exported)

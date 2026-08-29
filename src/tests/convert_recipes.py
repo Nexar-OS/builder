7b282e70a386528d8a511ca976a33886fea18435
c4vxl <manage@c4vxl.de>
2026-08-29 16:30:30 +0200

    Added notice for custom hooks

--- src/tests/convert_recipes.py
+++ src/tests/convert_recipes.py
@@ -45,6 +45,9 @@ dst.mkdir(exist_ok=True, parents=True)
 
 preset = Path("src/recipe/.example").read_text()
 
+def _is_overridden(recipe: TargetRecipe, method: str) -> bool:
+    return getattr(type(recipe), method) is not getattr(TargetRecipe, method)
+
 def export_recipe(recipe: TargetRecipe) -> str | None:
     if not any(isinstance(recipe.build_system, c) for c in [
         Autotools,
@@ -67,6 +70,11 @@ def export_recipe(recipe: TargetRecipe) -> str | None:
     install_args = recipe.build_system.install_args or []
     install_args = "\n      - ".join(install_args) + "\n"
 
+    python_notice = "See in python code!"
+    patches = python_notice if _is_overridden(recipe, "patch") else "<patches>"
+    post_install = python_notice if _is_overridden(recipe, "post_install") else "<post_install>"
+    prepare = python_notice if _is_overridden(recipe, "prepare") else "<prepare>"
+
     return preset \
         .replace("<name>", recipe.name) \
         .replace("<version>", recipe.version) \
@@ -80,10 +88,16 @@ def export_recipe(recipe: TargetRecipe) -> str | None:
         .replace("<build_method>", recipe.build_method.name) \
         .replace("<build_system>", type(recipe.build_system).__name__.lower()) \
         .replace("<disable_fakeroot>", f"{getattr(recipe.build_system, 'disable_fakeroot', False)}") \
+        .replace("<patches>", patches) \
+        .replace("<post_install>", post_install) \
+        .replace("<prepare>", prepare) \
         .replace("disable_fakeroot: False\n", "") \
         .replace("config_args:\n      - \n", "config_args:") \
         .replace("build_args:\n      - \n", "build_args:") \
         .replace("install_args:\n      - \n", "install_args:") \
+        .replace("\n  prepare: |\n    <prepare>\n", "") \
+        .replace("\n  patches:\n    - <patches>\n", "")
+
 
 for r in recipes:
     path: Path = dst / (r.name + ".yaml")

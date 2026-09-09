b9cfbf6f5037acdb575ba36701b01f3767cc232c
c4vxl <manage@c4vxl.de>
2026-09-09 10:53:20 +0200

    Added detect_ram and detect_parallelism helper functions

--- src/builder/build/machine.py
+++ src/builder/build/machine.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import subprocess
 import platform
+import math
 
 from dataclasses import dataclass
 
@@ -131,3 +132,63 @@ def detect_machine() -> MachineSpec:
         kernel_arch=kernel_arch_map.get(arch, arch),
         triple=detect_triple()
     )
+
+def detect_ram() -> float:
+    """
+    Reads MemTotal from /proc/meminfo.
+
+    Returns:
+        float: Returns the amount of ram of this machine (in GiB).
+    """
+    with open("/proc/meminfo", "r") as f:
+        for line in f:
+            if line.startswith("MemTotal:"):
+                kib = int(line.split()[1])
+                return kib / (1024 ** 2)
+    
+    raise RuntimeError("Unable to determine system ram.")
+
+def detect_parallelism(cores: int | None = None, ram: float | None = None, ram_per_recipe: int = 8) -> tuple[int, int]:
+    """Determine suitable build parallelism for the current system.
+    
+    The calculation aims to keep CPU at high utilization while limiting
+    the number of simultaneously active recipes to avoid memory and filesystem
+    contention.
+
+    Args:
+        cores (int | None): The amount of cores of the machine (defaults to whatever ``nproc`` returns).
+        ram (int | None): The amount of ram (in GiB) (defaults to whatever ``/proc/meminfo`` returns).
+        ram_per_recipe (int): A general assumption of how much ram each recipe will consume.
+
+    Returns:
+        tuple[int, int]: (max_workers, num_jobs)
+    """
+    cores = cores or nproc()
+    ram = ram or detect_ram()
+
+    # Prefer several independent recipes on larger machines
+    if cores <= 4:
+        max_workers = 1
+    elif cores <= 8:
+        max_workers = 2
+    elif cores <= 16:
+        max_workers = 3
+    elif cores <= 32:
+        max_workers = 4
+    else:
+        max_workers = 8
+
+    # Reduce concurrency on memory-constrained systems
+    max_workers = min(
+        max_workers,
+        max(1, math.floor(ram / ram_per_recipe))
+    )
+
+    # Slightly oversubscribe the CPU so that jobs waiting on I/O
+    # operations don't leave cores idle.
+    total_jobs = math.ceil(cores * 1.5)
+
+    # Divide available CPU parallelism between recipes
+    num_jobs = max(1, math.ceil(total_jobs / max_workers))
+
+    return max_workers, num_jobs

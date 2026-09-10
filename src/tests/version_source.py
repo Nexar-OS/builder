2208fb4861614872c477a108abea63bc627f5134
c4vxl <manage@c4vxl.de>
2026-09-10 14:26:40 +0200

    Added new version tests

--- src/tests/version_source.py
+++ src/tests/version_source.py
@@ -1,15 +1,18 @@
 from tests.vars import ctx
 from builder.recipe import BuildRole
 
-recipe = ctx.registry.getOrThrow("util-linux", BuildRole.TARGET, ctx)
+recipes = [
+    ctx.registry.getOrThrow(
+        name=recipe,
+        role=BuildRole.TARGET,
+        ctx=ctx
+    )
+    for recipe in sorted(ctx.registry.paths().keys())
+]
 
-source = recipe.version_source
-assert source
-
-print(
-    "Latest upstream version: " +
-    max(source.versions).raw
-)
-
-for version in source.versions:
-    print(version)
+for recipe in recipes:
+    version_source = recipe.version_source
+    
+    print(f"Loading: {recipe.name}")
+    version = version_source.latest_version if version_source else None
+    print(f"Recipe '{recipe.name}': {version or '/'}")

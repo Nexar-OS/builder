498f0a21a7332155ef26c992fcdfa9d83ea37084
c4vxl <manage@c4vxl.de>
2026-09-10 11:11:59 +0200

    Fixed test vars

--- src/tests/vars.py
+++ src/tests/vars.py
@@ -3,18 +3,25 @@ from builder.toolchain import NativeToolchain
 from builder.build import (
     BuildContext,
     detect_machine,
-    nproc,
+    detect_parallelism,
     Target
 )
+from builder.recipe import RecipeRegistry
+
+max_workers, num_jobs = detect_parallelism()
 
 ctx = BuildContext(
+    registry                = RecipeRegistry([
+                                    Path(__file__).parent.parent / "recipe",
+                                ]),
     build_dir               = Path("build").resolve(),
     staging_dir             = Path("build/staging").resolve(),
     metadata_dir            = Path("build/.metadata").resolve(),
     build_machine           = detect_machine(),
     target_machine          = Target.X86_64,
     toolchain               = NativeToolchain(),
-    cross_toolchain_dir     = Path("build/toolchain/binaries").resolve(),
-    cross_toolchain_sysroot = Path("build/toolchain/sysroot").resolve(),
-    num_jobs                = nproc()
+    toolchain_dir           = Path("build/toolchain/binaries").resolve(),
+    toolchain_sysroot       = Path("build/toolchain/sysroot").resolve(),
+    num_jobs                = num_jobs,
+    verbose_build_logs      = False,
 )

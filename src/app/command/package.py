14cad120ea34695b8bcf19e3041ad4f6af3ebfc8
c4vxl <manage@c4vxl.de>
2026-09-26 11:25:58 +0200

    Added --with-dependencies option to package command and implemented proper concurrency

--- src/app/command/package.py
+++ src/app/command/package.py
@@ -1,9 +1,11 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from dataclasses import dataclass
 
 from pathlib import Path
 
+from builder.recipe import DependencyGraph, DependencyKind
 from builder.build import BuildContext
-from builder.recipe import BuildRole
+from builder.recipe import BuildRole, BuildRecipe
 from builder.package import *
 from builder.package.export import *
 from builder.utils.logger import error, warn
@@ -23,6 +25,12 @@ class PackageCommand(CLICommand):
         type=bool,
         help="Select all available recipes over all repositories passed to the registry.",
         flags=("--all", "-a")
+    ).arg()
+
+    with_dependencies: bool = CLIArgument(
+        type=bool,
+        help="Export all runtime dependencies of packages as well.",
+        flags=("--with-deps", "-d")
     ).arg()
 
     export_path: Path | None = CLIArgument(
@@ -57,26 +65,54 @@ class PackageCommand(CLICommand):
 
         if not recipe_names:
             error("Please pass one or more recipes to package.")
-
-        for recipe_name in recipe_names:
-            recipe = ctx.registry.get(
+        
+        recipes = [
+            ctx.registry.getOrThrow(
                 name=recipe_name,
                 role=BuildRole.TARGET,
                 ctx=ctx
             )
+            for recipe_name in recipe_names
+        ]
 
-            if not recipe:
-                continue
-
-            package = Package.from_recipe(recipe)
-
-            if not package:
-                warn(f"Cannot export recipe '{recipe.name}'. Recipe hasn't been built yet!")
-                continue
-
-            artifact = exporter.export(
-                package=package,
-                destination=self.export_path or ctx.build_dir / "exported"
+        # Add runtime dependencies
+        if self.with_dependencies:
+            graph = DependencyGraph(
+                recipes=recipes,
+                registry=ctx.registry,
+                kind=DependencyKind.RUNTIME,
+                allow_cycles=True
+            )
+            
+            recipes = list(
+                graph.recipes.values()
             )
 
-            print(f"Successfully packaged '{package.metadata.name}' into '{artifact.path}'")
+        with ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(
+                    self._package_recipe,
+                    recipe,
+                    exporter,
+                    self.export_path or ctx.build_dir / "exported"
+                )
+
+                for recipe in recipes
+            ]
+
+            for future in as_completed(futures):
+                future.result()            
+    
+    def _package_recipe(self, recipe: BuildRecipe, exporter: PackageExporter, export_dir: Path):
+        package = Package.from_recipe(recipe)
+
+        if not package:
+            warn(f"Cannot export recipe '{recipe.name}'. Recipe hasn't been built yet!")
+            return
+
+        artifact = exporter.export(
+            package=package,
+            destination=export_dir
+        )
+        
+        print(f"Successfully packaged '{package.metadata.name}' into '{artifact.path}'")
